2d6a66a392b86e42019291096aa7a98559c490b7
RaimisD <raimisdam@gmail.com>
2023-11-16 00:54:51 +0200

    started columns

--- force-app/main/default/lwc/csc/csc.js
+++ force-app/main/default/lwc/csc/csc.js
@@ -5,41 +5,56 @@ import { getListInfoByName, getListInfosByName } from "lightning/uiListsApi";
 import { getListUi } from 'lightning/uiListApi';
 
 import getBoardRecords from '@salesforce/apex/boardController.getBoardRecords';
+import getBoardColumns from '@salesforce/apex/boardController.getBoardColumns';
 
 import BOARD_OBEJCT from '@salesforce/schema/Board__c';
 
 export default class Csc extends LightningElement {
 
     @api boards;
+    @api columns;
     @api error;
     @api tabData;
 
-    
+
     connectedCallback() {
         this.loadBoards();
+        this.loadColumns();
     }
 
     loadBoards() {
         getBoardRecords()
-            .then(result => {
-                this.boards = result;
-                console.log('result: ', result);
-                this.boards = result;
-                this.error = undefined;
-            })
-            .catch(error => {
-                this.error = error;
-                this.boards = undefined;
-                console.log('error, no data');
-            });
+        .then(result => {
+            this.boards = result;
+            console.log('board result: ', result);
+            this.error = undefined;
+        })
+        .catch(error => {
+            this.error = error;
+            this.boards = undefined;
+            console.log('error, no data');
+        });
     }
-    processResults(data) {
-        this.tabData = data.map((board, index) => ({
-            id: board.Id,
-            name: board.Name,
-            tabIndex: index,
-            active: index === 0 // First tab active by default
-        }));
+    // processResults(data) {
+    //     this.tabData = data.map((board, index) => ({
+    //         id: board.Id,
+    //         name: board.Name,
+    //         tabIndex: index,
+    //         active: index === 0 // First tab active by default
+    //     }));
+    // }
+    loadColumns(){
+        getBoardColumns()
+        .then(result => {
+            this.columns = result;
+            console.log('column result: ', result);
+            this.error = undefined;
+        })
+        .catch(error => {
+            this.error = error;
+            this.columns = undefined;
+            console.log('error, no data');
+        });
     }
 
     @wire(getObjectInfo, {objectApiName: BOARD_OBEJCT})
