e853575d4802b93d3f1d0ee53c4b4f052d1d039c
RaimisD <raimisdam@gmail.com>
2023-11-15 23:02:37 +0200

    shows tabs at side

--- force-app/main/default/lwc/csc/csc.js
+++ force-app/main/default/lwc/csc/csc.js
@@ -1,10 +1,46 @@
-import { LightningElement, wire } from 'lwc';
+import { api, LightningElement, wire } from 'lwc';
 import { getRecord, getRecords, getRecordUi } from 'lightning/uiRecordApi';
 import { getObjectInfo, getObjectInfos } from 'lightning/uiObjectInfoApi';
 import { getListInfoByName, getListInfosByName } from "lightning/uiListsApi";
 import { getListUi } from 'lightning/uiListApi';
+
+import getBoardRecords from '@salesforce/apex/boardController.getBoardRecords';
+
 import BOARD_OBEJCT from '@salesforce/schema/Board__c';
+
 export default class Csc extends LightningElement {
+
+    @api boards;
+    @api error;
+    @api tabData;
+
+    
+    connectedCallback() {
+        this.loadBoards();
+    }
+
+    loadBoards() {
+        getBoardRecords()
+            .then(result => {
+                this.boards = result;
+                console.log('result: ', result);
+                this.boards = result;
+                this.error = undefined;
+            })
+            .catch(error => {
+                this.error = error;
+                this.boards = undefined;
+                console.log('error, no data');
+            });
+    }
+    processResults(data) {
+        this.tabData = data.map((board, index) => ({
+            id: board.Id,
+            name: board.Name,
+            tabIndex: index,
+            active: index === 0 // First tab active by default
+        }));
+    }
 
     @wire(getObjectInfo, {objectApiName: BOARD_OBEJCT})
     objInfo({error, data}){
