0f3c96bcd61eae7c1c72e2ba0cf5297ae1b72310
RaimisD <raimisdam@gmail.com>
2023-11-15 16:43:14 +0200

    Update csc.js

--- force-app/main/default/lwc/csc/csc.js
+++ force-app/main/default/lwc/csc/csc.js
@@ -1,3 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import { getRecord, getRecords, getRecordUi } from 'lightning/uiRecordApi';
+import { getObjectInfo, getObjectInfos } from 'lightning/uiObjectInfoApi';
+import { getListInfoByName, getListInfosByName } from "lightning/uiListsApi";
+import { getListUi } from 'lightning/uiListApi';
+import BOARD_OBEJCT from '@salesforce/schema/Board__c';
+export default class Csc extends LightningElement {
 
-export default class Csc extends LightningElement {}
+    @wire(getObjectInfo, {objectApiName: BOARD_OBEJCT})
+    objInfo({error, data}){
+        if(data){
+            console.log('object info: ',data);
+        }
+        else if(!data){
+            console.log('no data 1');
+        }
+        else if(error){
+            console.log('error: ', error);
+        }
+    }
+}
