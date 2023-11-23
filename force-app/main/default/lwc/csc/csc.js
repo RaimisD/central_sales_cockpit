c627be36853044ec1a82ca62105d76fb68ff9cef
RaimisD <raimisdam@gmail.com>
2023-11-23 15:02:32 +0200

    working links and icons

--- force-app/main/default/lwc/csc/csc.js
+++ force-app/main/default/lwc/csc/csc.js
@@ -1,34 +1,36 @@
 import { api, LightningElement, wire, track } from 'lwc';
-import { getRecord, getRecords, getRecordUi } from 'lightning/uiRecordApi';
-import { getObjectInfo, getObjectInfos } from 'lightning/uiObjectInfoApi';
-import { getListInfoByName, getListInfosByName } from "lightning/uiListsApi";
-import { getListUi } from 'lightning/uiListApi';
-
+import { NavigationMixin } from 'lightning/navigation'
 
 import getBoardRecords from '@salesforce/apex/boardController.getBoardRecords';
-//import getBoardColumns from '@salesforce/apex/boardController.getBoardColumns';
 import getColumRecords from '@salesforce/apex/boardController.getColumRecords';
-
-import BOARD_OBEJCT from '@salesforce/schema/Board__c';
-import BOARD_COLUMN_OBJECT from '@salesforce/schema/Board_column__c'
-import ACCOUNT_OBJECT from '@salesforce/schema/Account';
-import OPPORTUNITY_OBJECT from '@salesforce/schema/Opportunity';
-
-
-export default class Csc extends LightningElement {
-
-    @api boards;
-    @api columns;
-    @api error;
-    @api tabData;
-
+export default class Csc extends NavigationMixin(LightningElement) {
     connectedCallback() {
 
     }
-    @track boards;
-    @track error;
 
-    @wire(getBoardRecords)
+    navigateToRecord(event) {
+        event.stopPropagation();
+        const recordId = event.currentTarget.dataset.id;
+        this[NavigationMixin.GenerateUrl]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: recordId,
+                actionName: 'view'
+            },
+        }).then(url => {
+            window.open(url);
+        });
+    }
+
+    @track selectedTab;
+    handleTabSelect(event){ //checks which column is selected to pass value
+        this.selectedTab = event.target.value;
+        this.loadColumnRecords();
+        console.log('selected tab: ', event.target.value);
+    }
+    @api boards;
+    @track boards;
+    @wire(getBoardRecords) //loads boards
     wiredBoards({error, data}){
         if(data){
             console.log('board data: ',data)
@@ -42,37 +44,16 @@ export default class Csc extends LightningElement {
             console.log('no data 1');
         }
     }
-    
+    @api columns;
     @track columns;
-    @wire(getColumRecords)
-    wiredRecords({error, data}){
-        if(data){
-            console.log('column records: ',data);
-            this.columns = data;
-        }
-        else if(!data){
-            console.log('no data 3');
-            this.columns = undefined;
-        }
-        else if(error){
-            console.log('error 3: ', error);
-        }
-    }
-    @track recordUrl;
-    get recordUrl(){
-        return 'lightning/r/'
-    }
-
-    @wire(getObjectInfo, {objectApiName: BOARD_OBEJCT})
-    objInfo({error, data}){
-        if(data){
-            console.log('object info: ',data);
-        }
-        else if(!data){
-            console.log('no data 2');
-        }
-        else if(error){
-            console.log('error 2: ', error);
-        }
+    loadColumnRecords() { //loads columns and records
+        getColumRecords({ boardId: this.selectedTab })
+            .then(result => {
+                this.columns = result;
+                console.log('column data: ', result);
+            })
+            .catch(error => {
+                console.error(error);
+            });
     }
 }
