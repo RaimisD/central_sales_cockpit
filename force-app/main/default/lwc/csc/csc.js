4bf7956198f4ed0690aa3ef0727d0600c65ae6dc
RaimisD <raimisdam@gmail.com>
2023-11-24 14:53:38 +0200

    board and column adding modals

--- force-app/main/default/lwc/csc/csc.js
+++ force-app/main/default/lwc/csc/csc.js
@@ -1,14 +1,17 @@
 import { api, LightningElement, wire, track } from 'lwc';
 import { NavigationMixin } from 'lightning/navigation'
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 import getBoardRecords from '@salesforce/apex/boardController.getBoardRecords';
 import getColumRecords from '@salesforce/apex/boardController.getColumRecords';
 export default class Csc extends NavigationMixin(LightningElement) {
     connectedCallback() {
+        console.log("Board open? ", this.addBoardOpen);
+        console.log("Column open? ", this.addColumn);
 
     }
 
-    navigateToRecord(event) {
+    navigateToRecord(event) {  //generates link to record
         event.stopPropagation();
         const recordId = event.currentTarget.dataset.id;
         this[NavigationMixin.GenerateUrl]({
@@ -55,5 +58,53 @@ export default class Csc extends NavigationMixin(LightningElement) {
             .catch(error => {
                 console.error(error);
             });
+    }
+
+    //add new board
+    @track addBoardOpen = false;
+
+    handleAddBoard(event){
+        this.addBoardOpen = true;
+        console.log("Board open? ", this.addBoardOpen);
+    }
+    handleCloseAddBoard(event){
+        this.addBoardOpen = false;
+        console.log("Board open? ", this.addBoardOpen);
+
+    }
+    handleCreateBoardSuccess(event){
+        this.addBoardOpen = false;
+        const toastEvent = new ShowToastEvent({
+            title: 'Success',
+            message: 'Board created successfully',
+            variant: 'success'
+        });
+        this.dispatchEvent(toastEvent);
+    }
+    handleCreateBoardSubmit(event){
+        
+    }
+
+    //add new column
+    @track addColumn = false;
+    handleAddColumn(event){
+        this.addColumn = true;
+        console.log("column open? ", this.addColumn);
+    }
+    handleCloseAddColumn(event){
+        this.addColumn = false;
+        console.log("column open? ", this.addColumn);
+    }
+    handleAddColumnSuccess(event){
+        this.addColumn = false;
+        const toastEvent = new ShowToastEvent({
+            title: 'Success',
+            message: 'Column added successfully',
+            variant: 'success'
+        });
+        this.dispatchEvent(toastEvent);
+    }
+    handleAddColumnSubmit(event){
+
     }
 }
